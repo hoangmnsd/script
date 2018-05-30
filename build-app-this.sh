d5478615e58d6c895e7c36865a5c15b4f3ed76e0
hoangmnsd <hoangmnsd@gmail.com>
2018-05-30 23:26:27 +0700

    update build app this

--- build-app-this.sh
+++ build-app-this.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Install Package
-cd /var/lib/jenkins/workspace/2-freestyle-build-app-in-this-instance-medium/AWS_Demo/Angular/
+cd /var/lib/jenkins/workspace/2-build-app-in-cicd-instance/AWS_Demo/Angular/
 sudo npm install
 
 # Build App
-cd /var/lib/jenkins/workspace/2-freestyle-build-app-in-this-instance-medium/AWS_Demo/Angular/
+cd /var/lib/jenkins/workspace/2-build-app-in-cicd-instance/AWS_Demo/Angular/
 sudo npm run-script build
