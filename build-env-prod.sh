7760cc7b7dfb4e14bc0484abb5bfbfeec23c8e1d
hoangmnsd <hoangmnsd@gmail.com>
2018-05-30 16:14:09 +0700

    fix

--- build-env-prod.sh
+++ build-env-prod.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+
+# Install Npm - NodeJs
+sudo apt-get update
+sudo apt-get -y install nodejs
+curl -sL https://deb.nodesource.com/setup_8.x -o nodesource_setup.sh
+sudo bash nodesource_setup.sh
+sudo apt-get -y install nodejs
+
 # Install MySQL
 echo "America/Chicago" > /etc/timezone
 sudo dpkg-reconfigure -f noninteractive tzdata
@@ -23,7 +31,5 @@ aws s3 cp s3://awscicd/ /home/ubuntu/ --recursive
 mysql -hlocalhost -uroot -p awsdb < Dump20180523.sql --password="1111"
 
 #Install package npm in API
-cd /home/ubuntu/
 cd /home/ubuntu/API/
-pwd
 sudo npm install
