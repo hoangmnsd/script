6c25d6dd822e45cebb0f9fde37bbcce30eaecaec
hoangmnsd <hoangmnsd@gmail.com>
2018-05-30 15:16:57 +0700

    deploy 3

--- deploy.sh
+++ deploy.sh
@@ -4,19 +4,23 @@ sudo systemctl stop apache2
 sudo chgrp -R www-data /var/www/html
 sudo find /var/www/html -type d -exec chmod g+rx {} +
 sudo find /var/www/html -type f -exec chmod g+r {} +
-cd /home/ubuntu/AWS_Demo/Angular/dist
+cd /home/ubuntu/artifact-build-received/AWS_Demo/Angular
+npm install
+cd /home/ubuntu/artifact-build-received/AWS_Demo/Angular/dist
 sudo cp -Rv * /var/www/html
 cd /var/www/html
 sudo rm -rf assets
 sudo mkdir assets
-cd /home/ubuntu/AWS_Demo/Angular/dist/assets
+cd /home/ubuntu//artifact-build-received/AWS_Demo/Angular/dist/assets
 sudo cp -Rv * /var/www/html/assets
 sudo systemctl start apache2
 cd /var/run/screen/S-ubuntu
 sudo rm -rf *
-cd /home/ubuntu/AWS_Demo/API
+mkdir API
+cd /home/ubuntu/artifact-build-received/AWS_Demo/API
+npm install
 sudo kill $(sudo lsof -t -i:3000)
 screen -dmS RunService
-echo "sleeping 10 2222222222"
+echo "sleeping 10 2222222222 3333333333"
 sleep 10
 screen -S RunService -p 0 -X stuff 'node server.js\n'
